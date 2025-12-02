61c201884c0803704f32795ab4abab4014947943
kashishbhargava03 <kashish.bhargava@joshtechnologygroup.com>
2025-12-02 08:52:37 +0530

    A3-03:Added home page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export * from './errorBoundary';
 export * from './errorLayout';
+export * from './navbar';
+export * from './gridLayout';
+export * from './MovieGrid';
+export * from './movieCard';
 export * from './button';
