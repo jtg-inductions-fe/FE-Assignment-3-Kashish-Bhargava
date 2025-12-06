d73df8f52b3b4ef9f58d62d7c939aeacc1f5f346
kashishbhargava03 <kashish.bhargava@joshtechnologygroup.com>
2025-12-06 16:54:12 +0530

    A3-03:Updated button component

---
+++ src/components/errorLayout/ErrorLayout.component.tsx
@@ -0,0 +1,46 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@components';
+import { ROUTES } from '@constant';
+
+import {
+    ContentWrapper,
+    Description,
+    ImageWrapper,
+    Message,
+    StyledImage,
+    TextWrapper,
+    Wrapper,
+} from './ErrorLayout.styles';
+import { ErrorLayoutProps } from './ErrorLayout.types';
+
+export const ErrorLayout = ({
+    title,
+    description,
+    image,
+    altText,
+}: ErrorLayoutProps) => {
+    const navigate = useNavigate();
+
+    return (
+        <Wrapper>
+            <ImageWrapper>
+                <StyledImage src={image} alt={altText} />
+            </ImageWrapper>
+
+            <ContentWrapper>
+                <TextWrapper>
+                    <Message>{title}</Message>
+                    <Description>{description}</Description>
+                </TextWrapper>
+
+                <Button
+                    onClick={() => void navigate(ROUTES.HOME)}
+                    variant="contained"
+                >
+                    Go Back Home
+                </Button>
+            </ContentWrapper>
+        </Wrapper>
+    );
+};
